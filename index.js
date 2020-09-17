9d9bc1bdb73c4ae505b0eec238ccaf1ddd41e4ad
will Fletcher <wfletch7@gmail.com>
2020-09-17 15:56:26 -0400

    commit 1: on task 5

--- index.js
+++ index.js
@@ -41,9 +41,33 @@ class Airplane {
 */
 
 class Person {
+  constructor(attr){
+    this.name = attr.name;
+    this.age = attr.age;
+    this.stomach = []
+    }
+    eat(food){
+      if(this.stomach.length < 10){
+      return this.stomach.push(food);
+      };
+    }
+    poop(){
+      return this.stomach = [];
+    }
+    toString(){
+    return `${this.name}, ${this.age}`
+  }
 
 }
-
+const newPerson = new Person({
+  name: "William",
+  age: '21',
+});
+// console.log(newPerson)
+// console.log(newPerson.eat('potato'));
+// console.log(newPerson)
+// console.log(newPerson.poop());
+// console.log(newPerson.toString());
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -59,8 +83,39 @@ class Person {
 */
 
 class Car {
+  constructor(attributes){
+    this.model = attributes.model;
+    this.milesPerGallon = attributes.milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    return this.tank = (this.tank + gallons);
+  }
+  drive(distance){
+    if(this.tank <= distance / this.milesPerGallon){
+      this.odometer += this.tank / this.milesPerGallon;
+      this.tank = 0;
+      return `poor kid emptied his tank at ${this.odometer} miles`
+      }else {
+    this.odometer = this.odometer + distance
+    this.tank = this.tank - (distance / this.milesPerGallon);
+      };
 
 }
+}
+
+const dreamCar = new Car({
+  model: "lambo",
+  milesPerGallon: 50,
+  tank: 0,
+  odometer: 0,
+
+});
+// dreamCar.fill(50);
+// dreamCar.drive(70);
+// console.log(dreamCar);
+
 
 /*
   TASK 3
@@ -75,9 +130,18 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak(){
+    return `Hello my name is {name}, I am from {location}`
+  }
+
 
 }
-
+console.log(Lambdasian);
 /*
   TASK 4
     - Write an Instructor class extending Lambdasian.
@@ -92,10 +156,33 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+      constructor(attr){
+        super(attr)
+        this.specialty = attr.specialty;
+        this.favLanguage = attr.favLanguage;
+        this.catchPhrase = attr.catchPhrase;
 
+      }
+      demo(subject){
+        return `Today we are learning about ${subject}`;
+      }
+      grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}`;
+      }
 }
 
+const testInstructor = new Instructor({
+  name: 'josh',
+  age: 25,
+  location: "spain",
+  specialty: "redux",
+  favLanguage: 'notjavascript',
+  catchPhrase: 'wow i cant wait to move on',
+});
+// console.log(testInstructor);
+// console.log(testInstructor.demo("math"));
+// console.log(testInstructor.grade("andrew", "math"))
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
@@ -111,8 +198,16 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(attr){
+    super(attr);
+    this.previousBackground = attr.previousBackground;
+    this.className = attr.className;
+    this.favSubjects = attr.favSubjects;
+  }
+  listSubjects(){
+    return this.favSubjects.toString
+  }
 }
 
 /*
